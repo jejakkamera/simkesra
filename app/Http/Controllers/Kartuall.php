ee7ba544d099f4a17bd85dd902003c1da48da88b
adi rizky pratama <kurcaci507@gmail.com>
2025-03-25 21:36:50 +0700

    all

--- app/Http/Controllers/Kartuall.php
+++ app/Http/Controllers/Kartuall.php
@@ -23,74 +23,35 @@ class Kartuall extends Controller
     public function tandaterima(Request $request)
     {   
         $validated = $request->validate([
-            'kecamatan_id' => 'required',
-            'status_cetak' => 'required',
+            'kecamatan_id' => 'required|exists:wilayah_kec,id_wil',
         ]);
 
         // Ambil datanya
         $kecamatanId = $validated['kecamatan_id'];
-        $statusCetak = $validated['status_cetak'];
-
         $this->uuid = request()->query('UserId');
-
         $pivotQuery = Pemenangan::query()
-        ->join('profiles', 'profiles.id', '=', 'pemenangan.profile_id')
-        ->join('wilayah_kec', 'profiles.kode_kecamatan', '=', 'wilayah_kec.id_wil')
-        ->join('bantuan', 'bantuan.id', '=', 'pemenangan.idbantuan');
-
-    // Filter kecamatan jika tidak memilih "all"
-    if ($kecamatanId !== 'all') {
-        $pivotQuery->where('wilayah_kec.id_wil', $kecamatanId);
-    }
-
-    // Filter status_cetak jika tidak memilih "all"
-    if ($statusCetak !== 'all') {
-        $pivotQuery->where('pemenangan.status_cetak', $statusCetak == 1 ? 'Selesai' : '-');
-    }
-
-    $pivotQuery = $pivotQuery
-        ->orderBy('profiles.desa', 'ASC')
-        ->orderBy('bantuan.judul', 'ASC')
-        ->orderBy('profiles.nama_lengkap', 'ASC')
-        ->select(
-            'pemenangan.*',
-            'profiles.nama_lengkap',
-            'profiles.nik',
-            'profiles.tempat_mengajar',
-            'profiles.desa',
-            'profiles.alamat',
-            'wilayah_kec.nm_wil',
-            'bantuan.judul as bantuan_judul',
-            'bantuan.nominal as bantuan_nominal',
-            'bantuan.wilayah as bantuan_wilayah',
-            'pemenangan.id as uuid'
-        )
-        ->get()
-        ->groupBy('desa');
-
-        // $pivotQuery = Pemenangan::query()
-        //         ->join('profiles', 'profiles.id', '=', 'pemenangan.profile_id')
-        //         ->join('wilayah_kec', 'profiles.kode_kecamatan', '=', 'wilayah_kec.id_wil')
-        //         ->join('bantuan', 'bantuan.id', '=', 'pemenangan.idbantuan') 
-        //         ->where('wilayah_kec.id_wil', $kecamatanId)
-        //         ->orderBy('profiles.desa','ASC')
-        //         ->orderBy('bantuan.judul','ASC') // lalu urut bantuan
-        //         ->orderBy('profiles.nama_lengkap','ASC') // lalu urut nama
-        //         ->select(
-        //             'pemenangan.*',
-        //             'profiles.nama_lengkap',
-        //             'profiles.nik',
-        //             'profiles.tempat_mengajar',
-        //             'profiles.desa',
-        //             'profiles.alamat',
-        //             'wilayah_kec.nm_wil',
-        //             'bantuan.judul as bantuan_judul',
-        //             'bantuan.nominal as bantuan_nominal',
-        //             'bantuan.wilayah as bantuan_wilayah',
-        //             'pemenangan.id as uuid'
-        //         )
-        //         ->get()
-        //         ->groupBy('desa'); 
+                ->join('profiles', 'profiles.id', '=', 'pemenangan.profile_id')
+                ->join('wilayah_kec', 'profiles.kode_kecamatan', '=', 'wilayah_kec.id_wil')
+                ->join('bantuan', 'bantuan.id', '=', 'pemenangan.idbantuan') 
+                ->where('wilayah_kec.id_wil', $kecamatanId)
+                ->orderBy('profiles.desa','ASC')
+                ->orderBy('bantuan.judul','ASC') // lalu urut bantuan
+                ->orderBy('profiles.nama_lengkap','ASC') // lalu urut nama
+                ->select(
+                    'pemenangan.*',
+                    'profiles.nama_lengkap',
+                    'profiles.nik',
+                    'profiles.tempat_mengajar',
+                    'profiles.desa',
+                    'profiles.alamat',
+                    'wilayah_kec.nm_wil',
+                    'bantuan.judul as bantuan_judul',
+                    'bantuan.nominal as bantuan_nominal',
+                    'bantuan.wilayah as bantuan_wilayah',
+                    'pemenangan.id as uuid'
+                )
+                ->get()
+                ->groupBy('desa'); 
 
             return view('livewire.apps.penerima.bantuan.beritaacara',compact('pivotQuery'));
             
@@ -98,16 +59,19 @@ class Kartuall extends Controller
 
     public function indexkec(Request $request){
         $validated = $request->validate([
-            'kecamatan_id' => 'required|exists:wilayah_kec,id_wil',
+            'kecamatan_id' => 'required',
+            'status_cetak' => 'required',
         ]);
 
-        // Ambil datanya
+         // Ambil parameter filter
         $kecamatanId = $validated['kecamatan_id'];
+        $statusCetak = $validated['status_cetak'];
+
+        // Query utama
         $pivotQuery = Pemenangan::query()
             ->join('profiles', 'profiles.id', '=', 'pemenangan.profile_id')
             ->join('wilayah_kec', 'profiles.kode_kecamatan', '=', 'wilayah_kec.id_wil')
-            ->join('bantuan', 'bantuan.id', '=', 'pemenangan.idbantuan') 
-            ->where('wilayah_kec.id_wil',$kecamatanId)
+            ->join('bantuan', 'bantuan.id', '=', 'pemenangan.idbantuan')
             ->select(
                 'pemenangan.*',
                 'profiles.nama_lengkap',
@@ -120,20 +84,29 @@ class Kartuall extends Controller
                 'bantuan.wilayah as bantuan_wilayah',
                 'pemenangan.id as uuid'
             );
-        
-        // $pivotQuery = Pemenangan::join('profiles', 'profiles.id', '=', 'pemenangan.profile_id')->
-        //                         join('wilayah_kec', 'profiles.kode_kecamatan', '=', 'wilayah_kec.id_wil')
-        //                             ->select('pemenangan.*', 'profiles.*','wilayah_kec.*','pemenangan.id as uuid') // Sesuaikan dengan kolom yang ingin diambil
-        //                             ->with(['period', 'skema']);
 
-        if(session('active_role')=='unit'){
+        // Filter kecamatan jika tidak memilih "all"
+        if ($kecamatanId !== 'all') {
+            $pivotQuery->where('wilayah_kec.id_wil', $kecamatanId);
+        }
+
+        // Filter status cetak jika tidak memilih "all"
+        if ($statusCetak !== 'all') {
+            $pivotQuery->where('pemenangan.status_cetak', $statusCetak === 'sudah' ? 'Selesai' : '-');
+        }
+
+        // Filter berdasarkan role "unit"
+        if (session('active_role') == 'unit') {
             $filterIds = UserBantuan::query()
                 ->where('user_id', auth()->user()->id)
-                ->pluck('bantuan_id') // Ambil kolom yang dipakai buat filtering
+                ->pluck('bantuan_id')
                 ->toArray();
 
-                $pivotQuery->whereIn('bantuan.id', $filterIds);
+            $pivotQuery->whereIn('bantuan.id', $filterIds);
         } 
+
+        // Ambil hasil query
+        $this->profile = $pivotQuery->get();
         $this->profile = $pivotQuery->get();
         return view('livewire.apps.penerima.bantuan.kartu',[
             'profiles' => $this->profile
