db1bf28c7d4a781d1d302be03d09031a6a56b3c0
TA Karawang Cerdas 2023 <kurcaci507@gmail.com>
2025-12-03 12:39:05 +0700

    chore: Remove trailing slashes from validator routes for consistency

--- routes/validator.php
+++ routes/validator.php
@@ -17,8 +17,8 @@ Route::middleware(['auth', 'checkActiveRole:validator'])->group(function () {
     Route::get('/apps/period/validator/ViewDashboard', ViewDashboard::class)->name('validator.PeriodDashboardBank');
     Route::get('/apps/period/validator/Pemenangan', PemenanganDatalist::class)->name('validator.PemenanganDatalist');
     Route::get('/apps/period/validator/PemenanganCreate', PemenanganCreate::class)->name('validator.PemenanganCreate');
-    Route::get('/apps/period/validator/bukti/', PemenanganBukti::class)->name('validator.PemenanganBukti');
-    Route::get('/apps/period/validator/report/', PemenanganReport::class)->name('validator.PemenanganReport');
+    Route::get('/apps/period/validator/bukti', PemenanganBukti::class)->name('validator.PemenanganBukti');
+    Route::get('/apps/period/validator/report', PemenanganReport::class)->name('validator.PemenanganReport');
     Route::get('/apps/profile/add', ProfileCreate::class)->name('validator.ProfileCreate');
 
 });
